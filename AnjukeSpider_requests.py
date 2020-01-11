0c63c3895aadfde262f6848e1a57a1474528ca5a
Junpeng <623968625@qq.com>
2020-01-11 23:59:17 +0800

    尝试1

--- AnjukeSpider_requests.py
+++ AnjukeSpider_requests.py
@@ -18,7 +18,7 @@ def start_requests(url):
     return rep.content
 
 
-def parse(content):
+def parse(content, old_url):
     soup = BeautifulSoup(content, 'html.parser')
     link_list = soup.find_all("a", attrs={"class": "houseListTitle"})
 
@@ -27,19 +27,18 @@ def parse(content):
         my_dict['title'] = link['title']
         my_dict['href'] = link['href']
         result_list.append(my_dict)
-        next_page = soup.find('a', class_='aNxt')
-        if next_page:
-            time.sleep(3)
-            next_url = next_page['href']
-            text = start_requests(next_url)
-            parse(text)
-            write_json(result_list)
-        else:
-            print("防爬机制已阻拦")
-            next_url_bak = next_page['href']
-            driver = webdriver.Chrome()
-            driver.get(next_url_bak)
-            driver.maximize_window()
+    next_page = soup.find('a', class_='aNxt')
+    if next_page:
+        time.sleep(3)
+        next_url = next_page['href']
+        text = start_requests(next_url)
+        parse(text, next_url)
+        write_json(result_list)
+    else:
+        print("防爬机制已阻拦，需要人工操作")
+        driver = webdriver.Chrome()
+        driver.get(old_url)
+        driver.maximize_window()
 
 
 def write_json(result):
@@ -54,9 +53,9 @@ if __name__ == '__main__':
     base_url = "https://beijing.anjuke.com/sale/p1-v3/#filtersort"
     try:
         base_text = start_requests(base_url)
-        parse(base_text)
+        parse(base_text, base_url)
         # write_json(result_list)
-    except TimeoutError:
+    except Exception:
         pass
     finally:
         write_json(result_list)
