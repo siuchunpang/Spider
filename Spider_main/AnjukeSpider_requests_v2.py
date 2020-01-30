6e645570a47bbb080063489b43e437eee9bd7a19
Junpeng <623968625@qq.com>
2020-01-30 22:03:19 +0800

    使用ip线程池还是会出现爬虫验证，需要补充相应处理代码

--- Spider_main/AnjukeSpider_requests_v2.py
+++ Spider_main/AnjukeSpider_requests_v2.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from selenium import webdriver
 import time
+import random
 import json
 
+ua = UserAgent()
+spider_count = 1
 
-# 代理ip线程池api
+
+# 代理ip线程池
 def get_proxy():
     return requests.get("http://127.0.0.1:5010/get/").json()
 
@@ -15,15 +18,25 @@ def delete_proxy(proxy):
     requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
 
 
+# 爬虫相关
 def get_html(url):
+    USER_AGENT = ua.random
+    headers = {
+        "User-Agent": USER_AGENT,
+        "Connection": "keep-alive",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "zh-CN,zh;q=0.8"}
+
     retry_count = 5  # 容错次数
     proxy = get_proxy().get("proxy")  # 获取代理ip
-    print(proxy)
+    print("开始第%d次爬虫" % spider_count)
+    print("代理ip为：" + str(proxy))
+    print("User-agent：" + USER_AGENT)
     while retry_count > 0:
         try:
-            html = requests.get(url, proxies={"http": "http://{}".format(proxy)})
+            html = requests.get(url, headers=headers, proxies={"http": "http://{}".format(proxy)})
             # 使用代理访问
-            return html
+            return html.content
         except Exception:
             retry_count -= 1
     # 出错5次, 删除代理池中代理
@@ -31,40 +44,31 @@ def get_html(url):
     return None  # 如果请求失败则返回None
 
 
-# 爬虫api
-def start_requests(url):
-    USER_AGENT = ua.random
-    print(USER_AGENT)
-    headers = {
-        "User-Agent": USER_AGENT,
-        "Connection": "keep-alive",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8"}
-    rep = requests.get(url, headers=headers)
-    return rep.content
-
-
-def parse(content, old_url):
+def parse(content):
+    global spider_count
     soup = BeautifulSoup(content, 'html.parser')
     link_list = soup.find_all("a", attrs={"class": "houseListTitle"})
+    print("开始解析...")
 
     for link in link_list:
         my_dict = {}
         my_dict['title'] = link['title']
         my_dict['href'] = link['href']
         result_list.append(my_dict)
+
+    print("解析完成！")
     next_page = soup.find('a', class_='aNxt')
     if next_page:
-        time.sleep(3)
+        spider_count += 1
+        time.sleep(random.random()*3)
         next_url = next_page['href']
-        text = start_requests(next_url)
-        parse(text, next_url)
-        write_json(result_list)
+        next_url_text = get_html(next_url)
+        parse(next_url_text)
     else:
-        print("防爬机制已阻拦，需要人工操作")
-        driver = webdriver.Chrome()
-        driver.get(old_url)
-        driver.maximize_window()
+        if spider_count == 50:
+            print("爬取完毕")
+        else:
+            print("防爬机制已阻拦，需要人工操作")
 
 
 def write_json(result):
@@ -74,17 +78,15 @@ def write_json(result):
 
 
 if __name__ == '__main__':
-    ua = UserAgent()
     result_list = []
-    base_url = "https://beijing.anjuke.com/sale/p1-v3/#filtersort"
+    base_url = "https://beijing.anjuke.com/sale/v3/"
     try:
-        # base_text = start_requests(base_url)
         base_text = get_html(base_url)
-        parse(base_text, base_url)
-        # write_json(result_list)
+        parse(base_text)
     except Exception:
         pass
     finally:
         write_json(result_list)
+        print(spider_count)
 
 
